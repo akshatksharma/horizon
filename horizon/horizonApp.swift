79aa2446a0508235484afa47bf7ee44b6af7fba2
Akshat Sharma <akshatksharma@wustl.edu>
2024-11-17 08:49:51 -0500

    Initial Commit

---
+++ horizon/horizonApp.swift
@@ -0,0 +1,17 @@
+//
+//  horizonApp.swift
+//  horizon
+//
+//  Created by Akshat Sharma on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct horizonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ horizonTests/horizonTests.swift
@@ -0,0 +1,17 @@
+//
+//  horizonTests.swift
+//  horizonTests
+//
+//  Created by Akshat Sharma on 11/17/24.
+//
+
+import Testing
+@testable import horizon
+
+struct horizonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
