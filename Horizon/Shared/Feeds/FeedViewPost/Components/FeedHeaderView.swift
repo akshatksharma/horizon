c9a249a5dc4499738a2047c18d643cae03433b3a
Akshat Sharma <akshatksharma@wustl.edu>
2025-05-24 14:01:11 -0400

    Offset repost header to get text inline with post body

--- Horizon/Shared/Feeds/FeedViewPost/Components/FeedHeaderView.swift
+++ Horizon/Shared/Feeds/FeedViewPost/Components/FeedHeaderView.swift
@@ -3,6 +3,7 @@ import ATProtoKit
 
 struct FeedHeaderView: View {
     let repostReason: ATUnion.ReasonRepostUnion?
+    @State private var repostIconWidth: CGFloat = 0
     
     var body: some View {
         if let repostReason = repostReason {
@@ -13,11 +14,15 @@ struct FeedHeaderView: View {
                         Image(systemName: "repeat")
                             .foregroundStyle(.secondary)
                             .font(.footnote)
+                            .readFrame { CGRect in
+                                repostIconWidth = CGRect.width
+                            }
                         Text("Reposted by \(displayName)")
                             .foregroundStyle(.secondary)
                             .font(.footnote)
                             .lineLimit(1)
                     }
+                    .offset(x: -((repostIconWidth / 2) + 2))
                 }
             case .reasonPin:
                 EmptyView()
